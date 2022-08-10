4cf483d1aaafc32df14a5e76901a4eb21b1b95af
phamk <phamkhoiedison@gmail.com>
2022-08-10 03:36:32 -0700

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
+
+  const url = '8ab2eeb364f972411c18856827ac96b5  '  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
